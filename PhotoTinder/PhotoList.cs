01ca5dfad0f4d7b51bfe82f75ef2a4b7083d8a98
Konrad Walas <kwalas314@gmail.com>
2020-02-14 14:23:03 +0100

    Copying works, before changing to windows forms

--- PhotoTinder/PhotoList.cs
+++ PhotoTinder/PhotoList.cs
@@ -1,42 +1,95 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Security;
+using System.Text.RegularExpressions;
 using System.Windows.Media.Imaging;
 
 namespace PhotoTinder
 {
     public class PhotoList
     {
-        private List<BitmapImage> listOfPhotos;
+        private Dictionary<Uri, BitmapImage> _listOfPhotos;
+        public int ActivePhotoIndex { get; private set; }
 
         public PhotoList()
         {
-            listOfPhotos = new List<BitmapImage>();
+            _listOfPhotos = new Dictionary<Uri, BitmapImage>();
         }
 
-        public BitmapImage GetPhoto(int photoIndex = 0)
+        public BitmapImage GetActivePhoto()
         {
-            return listOfPhotos[photoIndex];
+            if (_listOfPhotos.Count == 0) return null;
+            var photo = _listOfPhotos.ElementAt(ActivePhotoIndex);
+            return photo.Value;
         }
 
-        public void RemovePhoto(int photoIndex = 0)
+        public BitmapImage GetNextPhoto()
         {
-            listOfPhotos.Remove(listOfPhotos[photoIndex]);
+            if (_listOfPhotos.Count == 0) return null;
+            if (ActivePhotoIndex >= (_listOfPhotos.Count - 1))
+            {
+                ActivePhotoIndex = 0;
+            }
+            else
+            {
+                ActivePhotoIndex++;
+            }
+
+            var photo = _listOfPhotos.ElementAt(ActivePhotoIndex);
+            return photo.Value;
         }
 
-        public void AddPhoto(BitmapImage newImage)
+        public BitmapImage GetPreviousPhoto()
         {
-            listOfPhotos.Add(newImage);
+            if (_listOfPhotos.Count == 0) return null;
+            if (ActivePhotoIndex <= 0)
+            {
+                ActivePhotoIndex = _listOfPhotos.Count - 1;
+            }
+            else
+            {
+                ActivePhotoIndex--;
+            }
+
+            var photo = _listOfPhotos.ElementAt(ActivePhotoIndex);
+            return photo.Value;
+        }
+
+        public Uri GetActivePhotoUri()
+        {
+            if (_listOfPhotos.Count == 0) return null;
+            var photo = _listOfPhotos.ElementAt(ActivePhotoIndex);
+            return photo.Key;
+        }
+
+        public string GetActivePhotoFileName()
+        {
+            var temp = _listOfPhotos.ElementAt(ActivePhotoIndex).Key.OriginalString.Split('\\');
+            return temp[temp.Length - 1];
+        }
+
+        public void RemoveActivePhoto()
+        {
+            _listOfPhotos.Remove(_listOfPhotos.ElementAt(ActivePhotoIndex).Key);
+        }
+
+        public void AddPhoto(Uri fileUri)
+        {
+            _listOfPhotos.Add(fileUri, new BitmapImage(fileUri));
         }
 
         public int Length()
         {
-            return listOfPhotos.Count;
+            return _listOfPhotos.Count;
         }
 
         public void Clear()
         {
-            listOfPhotos.Clear();
+            _listOfPhotos.Clear();
+            ActivePhotoIndex = 0;
         }
     }
 }
---
+++ PhotoTinder/PhotoManager.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+
+//using Microsoft.Win32;
+
+namespace PhotoTinder
+{
+    public class PhotoManager
+    {
+        private PhotoList _listOfPhotos = new PhotoList();
+        private string _acceptedPhotosName = "Accepted Photos";      //folder name for accepted photos
+        private string _removedPhotosName = "Removed Photos";
+        public void ChoosePhotos()
+        {
+            _listOfPhotos.Clear();
+
+            var openFileDialog = new OpenFileDialog { Multiselect = true };
+
+            if (openFileDialog.ShowDialog() == true)            //load selected photos to _listOfPhotos
+            {
+                foreach (var openFile in openFileDialog.FileNames)
+                {
+                    var fileUri = new Uri(openFile);
+                    _listOfPhotos.AddPhoto(fileUri);
+                }
+            }
+
+            Trace.WriteLine("acceptedPhotosName : " + _acceptedPhotosName);                  //for debugging purposes
+            Trace.WriteLine("openFileDialog.FileName : " + openFileDialog.FileName);        //for debugging purposes
+            _acceptedPhotosName = Regex.Replace(openFileDialog.FileName, @"[^\\]*$", @"") + _acceptedPhotosName;      //set the path for accepted photos folder
+            _removedPhotosName = Regex.Replace(openFileDialog.FileName, @"[^\\]*$", @"") + _removedPhotosName;      //set the path for removed photos folder
+            Trace.WriteLine("acceptedPhotosName After : " + _acceptedPhotosName);            //for debugging purposes
+        }
+
+        public BitmapImage GetNextPhoto()
+        {
+            return _listOfPhotos.GetNextPhoto();
+        }
+
+        public BitmapImage GetPreviousPhoto()
+        {
+            return _listOfPhotos.GetPreviousPhoto();
+        }
+
+        public BitmapImage GetActivePhoto()
+        {
+            return _listOfPhotos.GetActivePhoto();
+        }
+
+        public void DeletePhoto()
+        {
+            if (!Directory.Exists(_removedPhotosName))
+                Directory.CreateDirectory(_removedPhotosName);
+
+//            File.Delete(_listOfPhotos.GetActivePhotoUri().AbsolutePath);
+//            _listOfPhotos.RemoveActivePhoto();
+
+            File.Copy(_listOfPhotos.GetActivePhotoUri().AbsolutePath, _removedPhotosName + @"\" + _listOfPhotos.GetActivePhotoFileName());
+
+            _listOfPhotos.RemoveActivePhoto();
+        }
+
+        /// <summary>
+        /// Moves active photo to folder with accepted photos. Creates that folder if it doesn't exist. Don't forget to update displayed photo (using getActivePhoto method).
+        /// </summary>
+        public void AcceptPhoto()       
+        {
+            if (!Directory.Exists(_acceptedPhotosName))
+                Directory.CreateDirectory(_acceptedPhotosName);
+
+            Trace.WriteLine("Folder path: " + _acceptedPhotosName);  //for debugging purposes
+            Trace.WriteLine("Active Photo Uri " + _listOfPhotos.GetActivePhotoUri());
+
+            File.Copy(_listOfPhotos.GetActivePhotoUri().AbsolutePath, _acceptedPhotosName + @"\" + _listOfPhotos.GetActivePhotoFileName());
+
+            _listOfPhotos.RemoveActivePhoto();
+        }
+    }
+}
